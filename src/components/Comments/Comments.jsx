38aab922f7e45525da8a045ad3129d97eedc6b58
Meghan Gunderson <megundersonmn@gmail.com>
2020-01-27 08:19:31 -0600

    comments component working

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -5,11 +5,40 @@ import { HashRouter as Router, Route } from 'react-router-dom';
 //import axios from 'axios';
 
 class Comments extends Component {
+    //set state - for each component
+    state = {
+        comments: '',
+    }
+    //make handleClick function that sends state to ReduxState
+    //REMOVE IF/ELSE for this section - not needed
+    //then proceed to next page
+    handleClick = () => {
+            this.props.dispatch({
+                type: 'SET_COMMENTS',
+                payload: this.state.comments
+            })
+            //go to next page
+            this.props.history.push('/review')
+
+    }
+
+    //make handleChange function
+    handleChange = (event) => {
+        this.setState({
+            comments: event.target.value
+        })
+    }
     
     render() {
         return (
             <div>
                 <h1>Any comments you want to leave?</h1>
+
+                <input type="text" onChange={
+                    (event) => this.handleChange(event)}>
+                </input>
+
+                <button onClick={this.handleClick}>NEXT</button>
             </div>
         );
     }
